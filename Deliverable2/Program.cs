57436d52ef3a076ce7ccf91051259b1c0dfd8d00
Steve Schaner <schanerst@gmail.com>
2021-08-24 13:15:45 -0400

    Deliverable 2 working.

--- Deliverable2/Program.cs
+++ Deliverable2/Program.cs
@@ -6,7 +6,64 @@ namespace Deliverable2
     {
         static void Main(string[] args)
         {
-            
+            Boolean finished = true;
+            string message;
+            string messageRepeat = "I'm sorry but you have already said that." + Environment.NewLine;
+            int helloCount = 0;
+            int supCount = 0;
+            int helloThereCount = 0;
+
+            Console.WriteLine("Hello and welcome to Deliverable 2." + Environment.NewLine + "In this deliverable, you will be chatting with a chat bot. The bot's vocabulary is limited so your conversation might not be too deep. Enjoy!" + Environment.NewLine);
+
+            do
+            {
+                Console.Write("What do you wish to say? ");
+                string input = Console.ReadLine();
+
+                // response to 'hello'
+                if (input.ToLower() == "hello")
+                {
+                    helloCount++;
+                    message = "Hi, good to see you." + Environment.NewLine;
+                    if (helloCount > 1)
+                    {
+                        message = messageRepeat;
+                    }
+                }
+                // response to 'bye'
+                else if (input.ToLower() == "bye")
+                {
+                    message = "Good bye!";
+                    finished = false;
+                }
+                // response to 'sup'
+                else if (input.ToLower() == "sup")
+                {
+                    supCount++;
+                    message = "I am good." + Environment.NewLine;
+                    if (supCount > 1)
+                    {
+                        message = messageRepeat;
+                    }
+                }
+                // response to 'hello there'
+                else if (input.ToLower() == "hello there")
+                {
+                    helloThereCount++;
+                    message = "General Kenobi" + Environment.NewLine;
+                    if (helloThereCount > 1)
+                    {
+                        message = messageRepeat;
+                    }
+                }
+                else
+                {
+                    message = "I have a limited vocabulary. Please say someting that I understand." + Environment.NewLine;
+                }
+
+                Console.WriteLine(message);
+
+            } while (finished);
         }
     }
 }
